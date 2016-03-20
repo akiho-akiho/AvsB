2ae6afc6b66cdd31b7b3fe4b26f757366766a5cc
akiho-akiho <2358596983@qq.com>
2016-03-20 19:56:28 +0800

    多线程了

--- vs.py
+++ vs.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 import time
 import threading
@@ -22,6 +23,7 @@ class Person:
         print self.name + "'s HP is " + str(self.health_point) + "."
 
 class timer(threading.Thread):
+# 多线程测试
     def __init__(self, num):
         threading.Thread.__init__(self)
         self.thread_num = num
@@ -34,8 +36,29 @@ class timer(threading.Thread):
     def stop(self):
         self.thread_stop = True
 
+class chatic_fight(threading.Thread ):
+    def __init__(self, fighter, accepter):
+        self.fighter = fighter
+        self.accepter = accepter
+        threading.Thread.__init__(self)
+        # self.thread_player = player
+        self.thread_stop = False
+
+    def run(self):
+        while(self.accepter.health_point > 0):
+            time.sleep(1)
+            attack = random.randint(0, 599)
+            self.accepter.health_point = self.accepter.health_point - attack
+            print self.fighter.name +' causes ' + str(attack) + ' point damage to' + self.accepter.name + '. The remain hp is about ' + str(self.accepter.health_point)
+        print '='*10 + self.fighter.name + ' wins!'
+        self.thread_stop()
+
+    def stop(self):
+        self.thread_stop = True
+
 
 def fight(player_1,player_2):
+# 你打我一下，我打你一下的最基本攻击模式
     while(player_1.health_point > 0 and player_2.health_point > 0):
         attack = random.randint(0, 599)
         print player_1.name + ' causes ' + str(attack) + ' point damage to ' + player_2.name
@@ -57,12 +80,23 @@ def test():
     thread2.start()
     time.sleep(10)
     thread1.stop()
-    thread2.stop()
+    thread3.stop()
 
+def chatic_fight_test():
+    p1 = raw_input("Player1:")
+    p2 = raw_input("Player2:")
+    player_1 = Person()
+    player_2 = Person()
+    player_1.set_name(p1)
+    player_2.set_name(p2)
+    player_1.get_health_point()
+    player_2.get_health_point()
+    thread1 = chatic_fight(player_1, player_2)
+    thread2 = chatic_fight(player_2, player_1)
+    thread1.start()
+    thread2.start()
 
-
-if __name__ == "__main__":
-    test()
+def simple_fight():
     p1 = raw_input("Player1:")
     p2 = raw_input("Player2:")
     player_1 = Person()
@@ -75,4 +109,8 @@ if __name__ == "__main__":
     #player_2.my_message()
     winner = fight(player_1, player_2)
     print winner.name + ' wins!! The ramaining blood is ' + str(winner.health_point)
+
+
+if __name__ == "__main__":
+    chatic_fight_test()
 
